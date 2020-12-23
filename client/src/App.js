3aee8b239a768559a1fc8489d074e08f80b6cd64
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-12-23 07:29:13 +0100

    add folder structure to  the front-end branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import withAuth from './withAuth';
-import Home from './Home';
-import Secret from './Secret';
-import Login from './Login';
+import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/secret">Secret</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
+const Hello = () => {
+  return  <h1>Hello World</h1>
+}
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/secret" component={withAuth(Secret)} />
-          <Route path="/login" component={Login} />
-        </Switch>
+function App() {
+  return (
+    <BrowserRouter> 
+      <div className="App">
+     {/* Navbar */}
+
+       <Switch>
+         <Route exact path="/" component={Hello} />
+       </Switch>
+
+      {/* footer */}
       </div>
-    );
-  }
+    </BrowserRouter>
+   
+  );
 }
 
 export default App;
