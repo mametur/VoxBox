47f15c9d40c2cf49a1e54512b08aa457503e1c9e
Anna <ikokianna@gmail.com>
2020-12-27 16:52:46 +0100

    modified from default index.js to only add the import bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from './components/homepage/Home';
+import NavBar from './components/navbar/navBar.jsx'
 import Footer from './footer/Footer'
+import './App.scss'
 
 class App extends Component {
   render() {
