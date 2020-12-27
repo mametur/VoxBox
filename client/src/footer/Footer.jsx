8625d96c5acf753c3af07411ec46cc06f1d80a91
Anna <ikokianna@gmail.com>
2020-12-27 21:05:38 +0100

    fixed email and website links, and logo display

--- client/src/footer/Footer.jsx
+++ client/src/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React, {
     Component
 } from "react";
 import logo_HYF from '../logo_HYF.svg';
-import logo from '../logoVox.png'
+import logoVox from '../logoVox.png'
 import { BsEnvelope } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
 
@@ -22,20 +22,17 @@ class Footer extends Component {
             <div className = "col-md-8" >
             <ul style={{listStyleType: 'none'}}>
             <li className = "card-title">Contact VoxBox</li>
-            <li><FaGithub /><a href = { this.state.repoUrl }  style={this.styles} > https: //github.com/gelilaa/VoxBox < /a></li>
-            <li><BsEnvelope /><a href = { this.state.email }  style={this.styles} > voxbox @gmail.com </a> </li>
+            <li><FaGithub /><a href = { this.state.repoUrl }  style={this.styles} > https://github.com/gelilaa/VoxBox </a></li>
+            <li><BsEnvelope /><a href = { this.state.email }  style={this.styles} > voxbox@gmail.com </a></li>
             </ul></div > 
             <div className = "col-md-4" style = {{margin: 'auto 0'}}>
-            <img src = {this.logo()} className='m-2'/>
-            <img src = { logo_HYF } style = {{height:'4em', width:'4em'}} 
+            <img src = {logoVox} style = {{height:'4em', width:'4em', borderRadius:'10px'}} 
+            alt = "Voxbox logo" className='m-4'/>
+            <img src = { logo_HYF } style = {{height:'5em', width:'5em'}} 
             alt = "HYF logo" /></div>
             </div >
         );
     }
-
-    logo() {
-        return <img src={logo} alt="VoxBox logo" />;
-    };
 }
 
 export default Footer;
