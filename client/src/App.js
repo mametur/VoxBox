9e4051d4f163c928ecb5f230d320077b46dc518f
Anna <ikokianna@gmail.com>
2020-12-27 16:56:57 +0100

    edited index.js and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home } from './components/homepage/Home';
-import NavBar from './components/navbar/navBar.jsx'
 import Footer from './footer/Footer'
-import './App.scss'
 
 class App extends Component {
   render() {
