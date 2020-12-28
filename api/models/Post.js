787e1e2b315d36b05118897e921134db5bc729af
Miroslav Veljanoski <veljanoski.miroslav@gmail.com>
2020-12-28 19:42:40 +0100

    Add Post.js file

---
+++ api/models/Post.js
@@ -0,0 +1,48 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../db/db.js");
+const User = require("./User.js");
+
+const Post = sequelize.define("post", {
+  post_id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  topic: {
+    type: Sequelize.STRING(250),
+    allowNull: false,
+  },
+  discription: {
+    type: Sequelize.STRING(400),
+    allowNull: false,
+  },
+  city: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  user_id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+  },
+  published: Sequelize.BOOLEAN,
+  // Timestamps
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
+});
+
+// create one-to-many relation
+//test
+User.hasMany(Post, {
+  as: "post",
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  as: "user",
+  foreignKey: "user_id",
+});
+
+
+
+module.exports = Post;
