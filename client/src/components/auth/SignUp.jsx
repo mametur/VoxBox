c6471d9aa9873247bb7099219ba15be745563d24
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-12-30 16:51:08 +0100

    cannected signup page with backend

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -5,8 +5,7 @@ import { Form, Button, Container, Row, Col} from 'react-bootstrap'
 const SignUp = () => {
 
   const [state, setState] = useState({
-    firstName: '',
-    lastName: '',
+    name: '',
     email: '',
     password: ''
   })
@@ -20,6 +19,23 @@ const SignUp = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log('submit', state)
+
+    const data = state;
+
+    fetch('/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 
 
@@ -29,8 +45,8 @@ const SignUp = () => {
     <Col xs sm md lg xl>
       <Form>
       <Form.Group>
-        <Form.Control type="text" placeholder="Name" id="firstName" onChange={handleChange}/>
-        <Form.Control type="text" placeholder="Last Name" id="lastName" onChange={handleChange}/>
+        <Form.Control type="text" placeholder="Name" id="name" onChange={handleChange}/>
+        {/* <Form.Control type="text" placeholder="Last Name" id="lastName" onChange={handleChange}/> */}
         <Form.Control type="email" placeholder="Email" id="email" onChange={handleChange} />
         <Form.Control type="password" placeholder="Password" id="password" onChange={handleChange} />
 
