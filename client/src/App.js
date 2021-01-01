1260888da8aa78ed5954acd4231b12775e3cb057
Mamé <mameazad.turgut@gmail.com>
2021-01-01 22:15:44 +0100

    Serving Apps with Client-Side Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './components/homepage/Home';
-import NavBar from './components/navbar/navBar.jsx'
-import Footer from './components/footer/Footer.jsx'
-import SignUp from './components/auth/SignUp.jsx'
-import SignIn from './components/auth/SignIn'
-import './App.scss'
+import NavBar from './components/navbar/navBar.jsx';
+import Footer from './components/footer/Footer.jsx';
+import SignUp from './components/auth/SignUp.jsx';
+import SignIn from './components/auth/SignIn.jsx';
+import './App.scss';
 import './index.css';
 
-
 function App() {
-  return (
-    <BrowserRouter> 
-      <div className="App">
-        <NavBar />
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<NavBar />
 
-       <Switch>
-         <Route exact path="/" component={Home} />
-         <Route path="/signup" component={SignUp} />
-         <Route path="/signin" component={SignIn} />
-       </Switch>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/signup" component={SignUp} />
+					<Route path="/signin" component={SignIn} />
+				</Switch>
 
-      {/* footer */}
-      <Footer />
-      </div>
-    </BrowserRouter>
-   
-  );
+				{/* footer */}
+				<Footer />
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
