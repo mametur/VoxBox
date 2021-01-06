128777e2b0ab617296cec5d60092cd37bcb34df1
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2021-01-06 15:30:01 +0100

    added add post form

--- client/src/components/newsfeed/Newsfeed.jsx
+++ client/src/components/newsfeed/Newsfeed.jsx
@@ -2,21 +2,33 @@ import React, {useState} from 'react'
 import {useSelector} from "react-redux"
 import {Redirect} from "react-router-dom"
 import { Cards } from './Cards'
+import CreatePost from './createPost'
 import { NewsfeedHeader } from './NewsfeedHeader'
 import { SearchBar } from './SearchBar'
 
 export const Newsfeed = () => {
 
     const [searchedValue, setSearchedValue] = useState();
+    const [postFormState, setPostFormState] = useState({
+        status: false
+    })
 
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+
+    const handlePostFormToggle = () =>{
+        setPostFormState({
+            ...postFormState,
+            status: !postFormState.status
+        })
+    }
 
     if(!isLoggedIn) return <Redirect to="/" />
 
     return (
         <div>
-            <NewsfeedHeader />
+            <NewsfeedHeader formToggle={handlePostFormToggle}/>
             <SearchBar searching={setSearchedValue}/>
+            {postFormState.status? <CreatePost formToggle={handlePostFormToggle}/> : null}
             <Cards searchedValue={searchedValue}/>
         </div>
     )
