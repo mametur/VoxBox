cdca81c4304e9828196163e17f5e2e936f2a6ec6
Anna Ikoki <annaikoki@Annas-MacBook-Air.local>
2021-01-06 20:35:21 +0100

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import SignUp from './components/auth/SignUp.jsx'
 import SignIn from './components/auth/signin.jsx'
 import { Newsfeed } from './components/newsfeed/Newsfeed';
 import HelpDetails from './components/profile/HelpDetails';
+import ProfileDetails from './components/profile/ProfileDetails';
 
 import './App.scss';
 import './index.css';
@@ -18,29 +19,55 @@ import { useDispatch } from 'react-redux'
 
 function App() {
 
-	const dispatch = useDispatch()
-		dispatch(checkAuthState())
-		dispatch(checkUserState());
-		
+    const dispatch = useDispatch()
+    dispatch(checkAuthState())
+    dispatch(checkUserState());
 
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<NavBar />
 
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/signup" component={SignUp} />
-					<Route path="/signin" component={SignIn} />
-					<Route exact path="/newsfeed" component={Newsfeed} />
-					<Route exact path="/help" component={HelpDetails} />
-				</Switch>
+    return ( <
+        BrowserRouter >
+        <
+        div className = "App" >
+        <
+        NavBar / >
 
-				{/* footer */}
-				<Footer />
-			</div>
-		</BrowserRouter>
-	);
+        <
+        Switch >
+        <
+        Route exact path = "/"
+        component = { Home }
+        /> <
+        Route path = "/signup"
+        component = { SignUp }
+        /> <
+        Route path = "/signin"
+        component = { SignIn }
+        /> <
+        Route exact path = "/newsfeed"
+        component = { Newsfeed }
+        /> <
+        Route exact path = "/help/1"
+        component = { HelpDetails }
+        />  <
+        Route exact path = "/help/1"
+        component = { HelpDetails }
+        /> 
+
+        <
+        Route exact path = "/profile/1"
+        component = { ProfileDetails }
+        /> 
+
+        <
+        /
+        Switch >
+
+        { /* footer */ } <
+        Footer / >
+        <
+        /div> < /
+        BrowserRouter >
+    );
 }
 
 export default App;
