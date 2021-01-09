09d09fd068048ab4bc3f8522e244ede8929f7c23
Mamé <mameazad.turgut@gmail.com>
2021-01-09 12:24:45 +0100

    rewirte static middeleware

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(bodyParser.json());
 // });
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-// app.get('/*', function (req, res) {
-// 	res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.use('/api', api);
 
