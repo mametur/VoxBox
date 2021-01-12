686685c6de375bc8cbde306c41f37305ab268b7e
oguz <oguzkarademir26@gmail.com>
2021-01-12 08:38:20 +0100

    all open help requests close #81

--- client/src/components/newsfeed/SearchBar.jsx
+++ client/src/components/newsfeed/SearchBar.jsx
@@ -6,7 +6,7 @@ export const SearchBar = (props) => {
     return (
         <Row className="search">
             <Col xs={12} sm={8} className="search-title">
-                All Help Request
+                All Open Help Request
             </Col>
             <Col xs={12} sm={4} className="search-input">
                 <Form.Control className="search-form" onChange={event => props.searching(event.target.value)} type="text" placeholder="Seach requests by Location" />
